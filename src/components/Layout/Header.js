1971ba2d4ff5ff9cdcd4c26be16791d7cd3d1cde
Derek Kweon <kdh4646@hanmail.net>
2023-09-14 01:08:33 -0700

    2023-09-14 setting main components

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+
+import classes from "./Header.module.css";
+
+import mealsImage from "../../assets/meals.jpg";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A tale full of delicious foods!" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
