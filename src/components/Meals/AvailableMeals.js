e10654c8a98616fd49464e659a14d6e04c376c42
Derek Kweon <kdh4646@hanmail.net>
2023-09-18 01:12:58 -0700

    2023-09-18 get data from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealIem/MealItem";
 
 const AvailableMeals = (props) => {
+  const [meals, setMeals] = useState([]);
+
+  //Include Http Request, useEffect() => Can't return promise
+  useEffect(() => {
+    //firebase
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-25d42-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      //data in JSON
+      const responseData = await response.json();
+
+      //loaded data for DOM
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      //set Meals
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
   //helper variable
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
